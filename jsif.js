c9851fd291e3a11326fa0d414e52d65bcfe4434d
Aaron Green <53711550+skyguy6153@users.noreply.github.com>
2023-05-07 12:16:57 -0400

    Add files via upload

---
+++ jsif.js
@@ -0,0 +1,17 @@
+var youLikeMeat = true; //delete the comment after this to see the next example
+/*
+var myNum = 10; // the variable myNum is assigned the value 1
+
+if (myNum > 10) { // stating if myNum (which you assigned the value 1) is equal to 10
+
+    document.write("My num is greater than 10"); // if the above if statement is true, then code will print "My num is equal to 10" to document
+} else{ // if the statement is false
+    
+    document.write("myNum is not equal to 10"); // if false, this is printed instead
+} */
+
+if (youLikeMeat) {
+    document.write("Here is the meat lover's menu!");
+} else {
+    document.write("Here is the vegetarian menu!");
+}
