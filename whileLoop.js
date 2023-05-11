06a79df321354246025e476f7626bc39ae991c4d
Aaron Green <53711550+skyguy6153@users.noreply.github.com>
2023-05-10 21:50:59 -0400

    Add files via upload

---
+++ whileLoop.js
@@ -0,0 +1,9 @@
+var age = 5; //index variable
+
+                                                                    // while this condition is true...
+while (age < 10) {                                                                                        //your condition goes in the parenthesis
+   console.log("Your age is less than 10.");
+   age++; // adds 1 to the variable, age, so it will eventually break out of loop.                        // ...execute this code. 
+}
+
+document.write("You are now over 10");
