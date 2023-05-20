6cd64f7ce563301bf75272e5861eec66d6e5ef7d
Aaron Green <53711550+skyguy6153@users.noreply.github.com>
2023-05-20 11:13:24 -0400

    Add files via upload
    Practice using the break statement and the continue statement.

---
+++ bandc.js
@@ -0,0 +1,40 @@
+/*for (i = 0; i < 10; i++) {
+
+    if( i === 5 || i === 3) {
+        
+        continue;
+    }
+    
+    console.log(i);
+
+    if(i===7) { // if i is equal to 7, break out of loop.
+        break;
+    }
+} */
+
+/*for (i = 0; i < 25; i++) {
+    
+    if( i === 9 || i === 17) {
+        continue; // 9 and 7 will be skipped in the loop
+    }
+    
+    console.log(i);
+
+    if(i === 21) {
+        break;
+    }
+} */
+
+for (b = 0; b < 100; b++) {
+
+    if(b === 67 || b === 36 || b === 48) {
+        continue;
+    }
+    
+    console.log(b);
+
+    if(b === 95) {
+        break;
+    }
+}
+    
